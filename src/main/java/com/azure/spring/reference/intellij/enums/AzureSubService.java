be3fcb08d4adc3513957c978022b55adfe462640
Shili Chen <v-shilichen@microsoft.com>
2022-11-21 16:33:32 +0800

    Remove inappropriate definitions for 'azure-sub-service'

--- src/main/java/com/azure/spring/reference/intellij/enums/AzureSubService.java
+++ src/main/java/com/azure/spring/reference/intellij/enums/AzureSubService.java
@@ -2,12 +2,10 @@ package com.azure.spring.reference.intellij.enums;
 
 public enum AzureSubService {
 
-    storage(AzureService.Storage),
     blobs(AzureService.Storage),
     queues(AzureService.Storage),
     fileShare(AzureService.Storage),
 
-    keyVault(AzureService.KeyVault),
     secrets(AzureService.KeyVault),
     certificates(AzureService.KeyVault),
     ;
